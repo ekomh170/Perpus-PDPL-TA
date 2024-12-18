1b9c7001d70f64a5b243ad13ff596ecd49de2c54
ilyasbdlzz <ilyaskenway@gmail.com>
2024-12-18 19:01:23 +0700

    fixed dashboard/profile

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -274,7 +274,7 @@
                                 @else
                                     <a
                                         href="{{ route('login') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        class="btn btn-sm btn-primary me-2 px-2 py-1"
                                     >
                                         Log in
                                     </a>
